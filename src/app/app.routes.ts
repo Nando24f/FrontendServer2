58790fcb0f05841218e82bcb53e940b2d4fe4005
Juan Villagrán Olivera <j.villagran03@ufromail.cl>
2025-06-02 03:21:41 -0400

    Clon de repo de front primero

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { ManagerDashboardComponent } from '../app/pages/manager-dashboard/manager-dashboard.component';
 import { EmployeeComponent } from './employee/employee.component';
 
 export const routes: Routes = [
