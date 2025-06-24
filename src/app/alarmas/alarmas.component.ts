89526c6199c914901aa519e97abea6c6bd49d16a
Juan Villagrán Olivera <j.villagran03@ufromail.cl>
2025-06-24 01:38:34 -0400

    Actualizacion usabilidad de ejemplos

--- src/app/alarmas/alarmas.component.ts
+++ src/app/alarmas/alarmas.component.ts
@@ -14,10 +14,6 @@ export class AlarmasComponent implements OnInit {
   alarmasActivas: any[] = [];
   alarmasMapa: any[] = [];
 
-  paginaActivas = 1;
-  paginaMapa = 1;
-  tamanio = 5;
-
   constructor(private alarmasService: AlarmasService) {}
 
   get marcadoresMapa() {
@@ -29,41 +25,12 @@ export class AlarmasComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.alarmasService.getAlarmasCriticas().subscribe(data => {
+    this.alarmasService.getAlarmasCriticas().subscribe((data: any[]) => {
       this.alarmasActivas = data;
     });
 
-    this.alarmasService.getAlarmasConUbicacion().subscribe(data => {
+    this.alarmasService.getAlarmasConUbicacion().subscribe((data: any[]) => {
       this.alarmasMapa = data;
     });
-  }
-
-  paginar(array: any[], pagina: number): any[] {
-    const inicio = (pagina - 1) * this.tamanio;
-    return array.slice(inicio, inicio + this.tamanio);
-  }
-
-  siguienteActivas(): void {
-    if (this.paginaActivas * this.tamanio < this.alarmasActivas.length) {
-      this.paginaActivas++;
-    }
-  }
-
-  anteriorActivas(): void {
-    if (this.paginaActivas > 1) {
-      this.paginaActivas--;
-    }
-  }
-
-  siguienteMapa(): void {
-    if (this.paginaMapa * this.tamanio < this.alarmasMapa.length) {
-      this.paginaMapa++;
-    }
-  }
-
-  anteriorMapa(): void {
-    if (this.paginaMapa > 1) {
-      this.paginaMapa--;
-    }
   }
 }
