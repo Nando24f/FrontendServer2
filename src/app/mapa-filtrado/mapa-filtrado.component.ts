35a9d82e1a1c625ddee79091a26319a0d255c696
Juan Villagrán Olivera <j.villagran03@ufromail.cl>
2025-06-24 01:18:51 -0400

    Actualizacion usabilidad de ejemplos

--- src/app/mapa-filtrado/mapa-filtrado.component.ts
+++ src/app/mapa-filtrado/mapa-filtrado.component.ts
@@ -1,56 +1,81 @@
 import { Component, OnInit } from '@angular/core';
 import { AlarmasService } from '../services/Alarmas.service';
-import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { MapaComponent } from '../mapa/mapa.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-mapa-filtrado',
   standalone: true,
-  imports: [FormsModule, CommonModule, MapaComponent],
+  imports: [CommonModule,FormsModule],
   templateUrl: './mapa-filtrado.component.html',
   styleUrls: ['./mapa-filtrado.component.css']
 })
 export class MapaFiltradoComponent implements OnInit {
-  categorias: string[] = [];
-  marcadoresFiltrados: any[] = [];
-
   filtroCategoria: string = '';
   filtroBusqueda: string = '';
-  filtroAutor: string = '';
   fechaInicio: string = '';
   fechaFin: string = '';
+  filtroAutor: number | null = null;
+
+  categorias: any[] = [];
+  marcadoresFiltrados: any[] = [];
 
   constructor(private alarmasService: AlarmasService) {}
 
   ngOnInit(): void {
     this.cargarCategorias();
-    this.cargarAlarmasIniciales();
+    this.cargarTodasLasAlarmas();
   }
 
   cargarCategorias(): void {
-    this.alarmasService.getCategoriasDistintas().subscribe(cats => {
-      this.categorias = cats.map(c => c.categoria);
+    this.alarmasService.getAlarmasPorCategoria('').subscribe(cats => {
+      this.categorias = cats;
     });
   }
 
-  cargarAlarmasIniciales(): void {
-    this.alarmasService.getMapa().subscribe(alarmas => {
-      this.marcadoresFiltrados = alarmas;
+  cargarTodasLasAlarmas(): void {
+    this.alarmasService.getMapa().subscribe(res => {
+      this.marcadoresFiltrados = res;
     });
   }
 
   aplicarFiltros(): void {
-    const filtros = {
-      categoria: this.filtroCategoria || undefined,
-      texto: this.filtroBusqueda || undefined,
-      fechaInicio: this.fechaInicio || undefined,
-      fechaFin: this.fechaFin || undefined,
-      usuario: this.filtroAutor || undefined
-    };
+    if (this.filtroBusqueda) {
+      this.buscarPorTexto();
+    } else if (this.filtroCategoria) {
+      this.consultarPorCategoria();
+    } else if (this.filtroAutor) {
+      this.consultarPorUsuario();
+    } else if (this.fechaInicio && this.fechaFin) {
+      this.consultarPorRango();
+    } else {
+      this.cargarTodasLasAlarmas();
+    }
+  }
 
-    this.alarmasService.getAlarmasFiltradas(filtros).subscribe((datos: any[]) => {
-      this.marcadoresFiltrados = datos;
+  consultarPorCategoria(): void {
+    this.alarmasService.getAlarmasPorCategoria(this.filtroCategoria).subscribe(res => {
+      this.marcadoresFiltrados = res;
+    });
+  }
+
+  consultarPorUsuario(): void {
+    if (!this.filtroAutor) return;
+    this.alarmasService.getAlarmasPorUsuario(this.filtroAutor).subscribe(res => {
+      this.marcadoresFiltrados = res;
+    });
+  }
+
+  consultarPorRango(): void {
+    if (!this.fechaInicio || !this.fechaFin) return;
+    this.alarmasService.getAlarmasPorRango(this.fechaInicio, this.fechaFin).subscribe(res => {
+      this.marcadoresFiltrados = res;
+    });
+  }
+
+  buscarPorTexto(): void {
+    this.alarmasService.buscarAlarmas(this.filtroBusqueda).subscribe(res => {
+      this.marcadoresFiltrados = res;
     });
   }
 }
